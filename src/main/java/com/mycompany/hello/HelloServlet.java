ac4d60fb6cf8311d00f555cd8256e8dda2b1274e
kaylan44 <guillaume.g44@hotmail.fr>
2022-07-11 13:30:49 +0200

    Update HelloServlet.java

--- src/main/java/com/mycompany/hello/HelloServlet.java
+++ src/main/java/com/mycompany/hello/HelloServlet.java
@@ -17,7 +17,7 @@ public class HelloServlet extends HttpServlet {
         out.println("<BODY>");
         out.println("<h1>Hello Servlet</h1>");
         out.println("The deployment of the WebApp is working fine !!");   
-        out.println("<iframe src='https://giphy.com/embed/ujTVMASREzuRbH6zy5' width='480' height='360' frameBorder='0' class='giphy-embed' allowFullScreen></iframe>");        
+        out.println("<p> <iframe src='https://giphy.com/embed/ujTVMASREzuRbH6zy5' width='480' height='360' frameBorder='0' class='giphy-embed' allowFullScreen></iframe> </p>");        
         out.println("<h5>Youpi</h5>");
         out.println("</BODY>");
         out.println("</HTML>");
