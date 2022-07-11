0b0987af8f8d0cfad33b35b716521add4c86ddfc
kaylan44 <guillaume.g44@hotmail.fr>
2022-07-11 18:06:13 +0200

    Update and rename ressource.tf to main.tf

---
+++ Terraform/main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "3.5.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file("<NAME>.json")
+
+  project = "<PROJECT_ID>"
+  region  = "europe-west1"
+  zone    = "europe-west1-b"
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+}
