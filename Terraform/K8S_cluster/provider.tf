907810be9285ba56ead1e661c8a0ff9b82b5b1bd
kaylan44 <guillaume.g44@hotmail.fr>
2022-07-12 12:06:55 +0200

    Update provider.tf

--- Terraform/K8S_cluster/provider.tf
+++ Terraform/K8S_cluster/provider.tf
@@ -5,3 +5,12 @@ provider "google" {
   region  = var.region
   zone    = var.zone
 }
+
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "~> 4.0"
+    }
+  }
+}
