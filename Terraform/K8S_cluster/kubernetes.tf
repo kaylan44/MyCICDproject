5b5b69c3968826f0bfc94f4eae2d9aa6ba2f6337
kaylan44 <guillaume.g44@hotmail.fr>
2022-07-12 16:47:38 +0200

    Update kubernetes.tf

--- Terraform/K8S_cluster/kubernetes.tf
+++ Terraform/K8S_cluster/kubernetes.tf
@@ -22,7 +22,7 @@ resource "google_container_node_pool" "primary_preemptible_nodes" {
 
   node_config {
     preemptible  = true
-    machine_type = "e2-small"
+    machine_type = "e2-micro"
 
     # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
     service_account = google_service_account.default.email
