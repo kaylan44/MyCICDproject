4171cf02bf83ba01bc407d1622163d8c62fc77a9
kaylan44 <guillaume.g44@hotmail.fr>
2022-07-12 11:07:05 +0200

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -15,6 +15,19 @@ provider "google" {
   zone    = "europe-west1-b"
 }
 
-resource "google_compute_network" "vpc_network" {
-  name = "terraform-network"
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "f1-micro"
+
+  boot_disk {
+    initialize_params {
+    image = "cos-cloud/cos-stable"    
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.vpc_network.default
+    access_config {
+    }
+  }
 }
