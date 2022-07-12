3c9daeba57590fae52ce28646b528b0190452d7d
kaylan44 <guillaume.g44@hotmail.fr>
2022-07-12 12:13:32 +0200

    Create variables.tf

---
+++ Terraform/K8S_cluster/variables.tf
@@ -0,0 +1,16 @@
+variable "project" { 
+  default = "ilkilab08"
+}
+
+variable "credentials_file" {
+  default = "ilkilab08-cred.json"
+}
+
+variable "region" {
+  default = "europe-west1"
+}
+
+variable "zone" {
+  default ="europe-west1-b"
+}
+
