36f889fbbf64af4be2bbf9a3b43bbad41ea571c3
kaylan44 <guillaume.g44@hotmail.fr>
2022-07-12 16:15:21 +0200

    Update firewalls.tf

--- Terraform/K8S_cluster/firewalls.tf
+++ Terraform/K8S_cluster/firewalls.tf
@@ -1,7 +1,6 @@
 # https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/compute_firewall
 resource "google_compute_firewall" "allow-ssh" {
   name    = "allow-ssh"
-  network = google_compute_network.main.name
 
   allow {
     protocol = "tcp"
