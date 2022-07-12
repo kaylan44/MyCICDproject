233c1aec78b2c8114b5d5d9605b8cefc1dc5b078
kaylan44 <guillaume.g44@hotmail.fr>
2022-07-12 16:55:43 +0200

    Update kubernetes.tf

--- Terraform/K8S_cluster/kubernetes.tf
+++ Terraform/K8S_cluster/kubernetes.tf
@@ -16,9 +16,9 @@ resource "google_container_cluster" "primary" {
 
 resource "google_container_node_pool" "primary_preemptible_nodes" {
   name       = "my-node-pool"
-  location   = var.region
+  location   = var.zone
   cluster    = google_container_cluster.primary.name
-  node_count = 1
+  node_count = 3
 
   node_config {
     preemptible  = true
