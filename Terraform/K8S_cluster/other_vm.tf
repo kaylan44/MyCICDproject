0a406f53592ee2d9d0d9fdd3acb4b03284fb59be
kaylan44 <guillaume.g44@hotmail.fr>
2022-07-13 13:30:38 +0200

    Update other_vm.tf

--- Terraform/K8S_cluster/other_vm.tf
+++ Terraform/K8S_cluster/other_vm.tf
@@ -1,3 +1,7 @@
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+}
+
 resource "google_compute_instance" "vm_instance_jenkins" {
   name         = "terraform-jenkins"
   machine_type = "e2-micro"
